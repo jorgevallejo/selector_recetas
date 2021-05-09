b95eeb4f496d9f78a739e8b7a8c8e0364bff685d
Jorge <jorvaor@protonmail.com>
2021-05-09 19:08:25 +0200

    Initial commit
    Reads the list of receipt names from an xlsx file, converts it into a character vector and returns seven names at random with equal probability.

---
+++ selector_recetas.R
@@ -0,0 +1,16 @@
+# Load libraries
+library(readxl)
+
+# Read receipt names from excel file
+recetas <- read_excel(path = "../ZZZ-lista comidas.xlsx",
+                      sheet = "Platos",
+                      col_names = FALSE,
+                      col_types = "text")
+
+# Change tibble into character vector
+recetas <- recetas$...1
+
+# Extract a sample of 3 receipt names
+(menu <- sample(recetas,
+               size = 7)
+)
