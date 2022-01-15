f35aabde58bc68333c88a268333e4cdc9a5bfa6d
Jorge Vallejo <jorvaor@gmail.com>
2022-01-15 12:26:52 +0100

    Fix a comment

--- selector_recetas.R
+++ selector_recetas.R
@@ -10,7 +10,7 @@ recetas <- read_excel(path = "../ZZZ-lista comidas.xlsx",
 # Change tibble into character vector
 recetas <- recetas$...1
 
-# Extract a sample of 3 receipt names
+# Extract a sample of receipt names
 (menu <- sample(recetas,
                size = 7)
 )
