f204fcf44023018869edcc4b11bd442d13fcda21
Jorge <jorvaor@protonmail.com>
2022-02-28 20:36:06 +0100

    Asks for a new receipt once every 10 tries (random)
    This way the number of receipts will be growing with time, but
    not excesivelly fast.

--- selector_recetas.R
+++ selector_recetas.R
@@ -12,24 +12,24 @@ recetas <- as.data.frame(recetas,
                          stringsAsFactors=FALSE)
 colnames(recetas) <- "Platos"
 
-# Add 'new receipt' to the "Platos" column
-recetas <- rbind(recetas, "Nueva receta")
-
 # Extract a sample of receipt names
 ## Create a vector of weights for probabilities
 ## All receipts get at first the same probability (1)
 weights <- rep(1, # base probability 
                length(recetas[,1])) # repeated as many times as are receipts
 
+# Crate an empty vector for menu
+menu <- vector(mode = "character", length = 7)
+
 ## Calculate probability so that a new receipt is the result of
 ## sampling once every ten tries
+if (sample(10,1) == 1) {
+  menu[1] <- "Nueva receta"
+}
 
-### Every try is seven samples
-### It means we want a new receipt to emerge once every 70 samples (1/70)
 
-new_receipt <- 
-
+# Generate menu of 7 receipts
 (menu <- sample(recetas[,1],
-               size = 7,
+               size = sum(menu == ""), # Empty positions in the vector
                prob = weights)
 )
